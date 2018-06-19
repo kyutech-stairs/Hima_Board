12813e8607efac19443121aaeea2933a86326b89
MasakiKomemoto <komemoto1997@gmail.com>
2018-06-19 09:45:53 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
+  get 'home/top'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resource :posts
+  
+  root "posts#index"
+  
+  
 end
