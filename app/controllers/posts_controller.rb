9a3947a16d58cede200e97b00fe342fe947c194d
MasakiKomemoto <komemoto1997@gmail.com>
2018-06-26 16:46:46 +0000

    func2 second commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,19 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(limit: :desc)
   end
   def show
      @post = Post.find_by(id: params[:id]) 
   end
   def new
+  end
+  def create
+    
+    @post = Post.new(name: params[:name],limit: params[:limit],purpose: params[:purpose])
+    
+    @post.save
+    
+    redirect_to("/posts/index")
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   
   get "posts/new" => "posts#new"
   
+  post "posts/create" => "posts#create"
+  
   get 'posts/:id' => 'posts#show'
   
   
