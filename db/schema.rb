fcd8f16f1477c8a566ac30249918fbcbcd4c3413
MasakiKomemoto <komemoto1997@gmail.com>
2018-07-01 08:40:32 +0000

    updating-user final commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180630172728) do
+ActiveRecord::Schema.define(version: 20180701060314) do
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20180630172728) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
